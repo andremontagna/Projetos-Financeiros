8b728dbe3f530a0bdcc2dccad56c0b8084c3feef
André Montagna <andremontagna@gmail.com>
2026-01-16 15:21:55 -0300

    Criação de arquivos
    começo do desenvolvimento do programa

---
+++ dados_pessoais.py
@@ -0,0 +1,8 @@
+import os
+
+
+PASTA_BASE = r"C:\Users\André Montagna\OneDrive\Pessoal\FINANÇAS"
+NOME_ARQUIVO = "B3_operacoes.xlsx"
+NOME_ABA = "geral"
+
+CAMINHO_ARQUIVO = os.path.join(PASTA_BASE, NOME_ARQUIVO)
